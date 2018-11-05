9667582f0abe0dce524a05d869c10210b7551051
Hasegawa Yohei <yoheybrains@gmail.com>
2018-11-05 22:36:32 +0900

    modified: index.html index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,65 @@
-import { fromEvent, of } from 'rxjs';
-import { map, mergeMap, startWith, tap } from 'rxjs/operators';
+import { fromEvent, combineLatest, from } from 'rxjs';
+import { startWith, map, switchMap, merge, tap, mergeMap } from 'rxjs/operators';
 
-const searchElement = document.querySelector('input#search');
-const outputElement = document.querySelector('ul.result');
+const refreshButton = document.querySelector('.refresh');
+const closeButton1 = document.querySelector('.close1');
+const closeButton2 = document.querySelector('.close2');
+const closeButton3 = document.querySelector('.close3');
 
-// 以下を編集して下さい
-/*
+const refreshClickStream = fromEvent(refreshButton, 'click');
+const close1ClickStream = fromEvent(closeButton1, 'click');
+const close2ClickStream = fromEvent(closeButton2, 'click');
+const close3ClickStream = fromEvent(closeButton3, 'click');
 
-やること:
-1. キーボートが押されたら(もしくは検索ボックスの値が変わった時)
-2. 検索ボックスの値を任意のAPIで検索し(demo.jsではgithubのリポジトリを検索しています)
-3. 表示する
+const requestStream = refreshClickStream.pipe(
+  startWith('startup click'),
+  map(() => {
+    const randomOffset = Math.floor(Math.random()*500);
+    return 'https://api.github.com/users?since=' + randomOffset;
+  })
+);
 
-npm run pr
-localhost:8000/index.htmlを開く
-index.html index.js style.scssを編集してください(保存したら手動でブラウザをリロードしてください)
-*/
+const responseStream = requestStream.pipe(
+  switchMap(requestUrl => fetch(requestUrl)),
+  mergeMap(r => r.json()),
+);
+
+function createSuggestionStream(closeClickStream) {
+  return closeClickStream.pipe(
+    merge(
+      combineLatest(
+        closeClickStream.pipe(startWith('startup click')),
+        responseStream,
+        (click, listUser) => listUser[Math.floor(Math.random() * listUser.length)]
+      ),
+      refreshClickStream.pipe(map(() => null))
+    ),
+    startWith(null)
+  );
+}
+
+const suggestion1Stream = createSuggestionStream(close1ClickStream);
+const suggestion2Stream = createSuggestionStream(close2ClickStream);
+const suggestion3Stream = createSuggestionStream(close3ClickStream);
+
+// Rendering ---------------------------------------------------
+function renderSuggestion(suggestedUser, selector) {
+    const suggestionEl = document.querySelector(selector);
+    if (suggestedUser === null) {
+        suggestionEl.style.visibility = 'hidden';
+    } else {
+        suggestionEl.style.visibility = 'visible';
+        const usernameEl = suggestionEl.querySelector('.username');
+        usernameEl.href = suggestedUser.html_url;
+        usernameEl.textContent = suggestedUser.login;
+        const imgEl = suggestionEl.querySelector('img');
+        imgEl.src = "";
+        imgEl.src = suggestedUser.avatar_url;
+    }
+}
+
+suggestion1Stream.subscribe(suggestedUser => renderSuggestion(suggestedUser, '.suggestion1'));
+
+suggestion2Stream.subscribe(suggestedUser => renderSuggestion(suggestedUser, '.suggestion2'));
+
+suggestion3Stream.subscribe(suggestedUser => renderSuggestion(suggestedUser, '.suggestion3'));
